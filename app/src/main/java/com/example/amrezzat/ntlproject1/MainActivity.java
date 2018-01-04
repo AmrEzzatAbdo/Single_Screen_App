bb323d6b547ae5e63e4c06ea36cede85259651a2
Amr Ezzat <amrezzat89@yahoo.com>
2018-01-04 14:27:23 -0800

    Initial commit

---
+++ app/src/main/java/com/example/amrezzat/ntlproject1/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.amrezzat.ntlproject1;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Typeface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
